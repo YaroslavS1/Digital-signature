d7b6f2fb06f8aa7a0d2a5e1124e8f9c73994f3bb
Ярослав Сухоруков <y.sukhorukov@iva-tech.ru>
2022-03-16 14:39:45 +0300

    hot fix

--- cryptomod.py
+++ cryptomod.py
@@ -7,36 +7,63 @@ import os
 PRIVATE_KEY = 'private_key.pem'
 PUBLIC_KEY = 'public_key.pem'
 FILE = 'example.txt'
-ELECTRONIC_SIGNATURE = 'signature.txt'
-
-
-def sanitaize_path(path, default_path):
-    return path if path != '' else default_path
+SIGNATURE = 'signature.txt'
 
 
 @click.command()
-@click.option('--path_to_public_key', default='', prompt='enter the path', help=f'path to store public key')
-@click.option('--path_to_the_file', default='', prompt='enter the path', help=f'path to file')
-@click.option('--path_to_the_signature', default='', prompt='enter the path', help=f'signature path')
-def verify_signature(path_to_public_key, path_to_the_file, path_to_the_signature):
-    _path_to_public = sanitaize_path(path_to_public_key, PUBLIC_KEY)
-    _path_to_the_file = sanitaize_path(path_to_the_file, FILE)
-    _path_to_the_signature = sanitaize_path(path_to_the_signature, ELECTRONIC_SIGNATURE)
+@click.option('-p_private', '--path_to_private_key', default=os.path.join('./', PRIVATE_KEY),
+              prompt='Enter path to private key', type=click.Path(exists=True, dir_okay=False, readable=True),
+              help=f'Path to private key')
+@click.option('-p_file', '--path_to_the_file', default=os.path.join('./', FILE), prompt='Enter path to file',
+              type=click.Path(exists=True, dir_okay=False, readable=True), help=f'Path to file')
+@click.option('-p_signature', '--path_to_the_signature', default=os.path.join('./', SIGNATURE),
+              prompt='Enter path to signature', type=click.Path(exists=False, dir_okay=False, readable=True),
+              help=f'Path to signature')
+def sign_file(path_to_private_key, path_to_the_file, path_to_the_signature):
+    h = SHA256.new()
+    with open(path_to_the_file, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            h.update(chunk)
 
     try:
-        f_key = open(_path_to_public, 'r')
+        f = open(path_to_private_key, 'r')
+        key = RSA.import_key(f.read())
+    except FileNotFoundError:
+        raise FileNotFoundError('Private key not found')
+
+    signature = pkcs1_15.new(key).sign(h)
+    f = open(path_to_the_signature, 'wb')
+    f.write(signature)
+    f.close()
+
+    click.echo(f'Signature saved - {path_to_the_signature}')
+
+
+@click.command()
+@click.option('-p_public', '--path_to_public_key', default=os.path.join('./', PUBLIC_KEY),
+              prompt='Enter path to public key', type=click.Path(exists=True, dir_okay=False, readable=True),
+              help=f'Path to public key')
+@click.option('-p_file', '--path_to_the_file', default=os.path.join('./', FILE), prompt='Enter path to file',
+              type=click.Path(exists=True, dir_okay=False, readable=True), help=f'Path to file')
+@click.option('-p_signature', '--path_to_the_signature', default=os.path.join('./', SIGNATURE),
+              prompt='Enter path to signature', type=click.Path(exists=True, dir_okay=False, readable=True),
+              help=f'Path to store signature')
+def verify_signature(path_to_public_key, path_to_the_file, path_to_the_signature):
+    print(path_to_public_key, path_to_the_file, path_to_the_signature)
+    try:
+        f_key = open(path_to_public_key, 'r')
         pubkey = RSA.import_key(f_key.read())
     except FileNotFoundError:
         raise FileNotFoundError('Private key not found')
 
     try:
-        f_signature = open(_path_to_the_signature, 'rb')
+        f_signature = open(path_to_the_signature, 'rb')
         signature = f_signature.read()
     except FileNotFoundError:
         raise FileNotFoundError('Signature not found')
 
     h = SHA256.new()
-    with open(_path_to_the_file, "rb") as f:
+    with open(path_to_the_file, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             h.update(chunk)
     try:
@@ -44,71 +71,46 @@ def verify_signature(path_to_public_key, path_to_the_file, path_to_the_signature
     except ValueError:
         raise ValueError('Signature is invalid')
     else:
-        click.echo(f'the signature for the file {_path_to_the_file} is valid')
+        click.echo(f'the signature for file {path_to_the_file} is valid')
 
 
 @click.command()
-@click.option('--path_to_private_key', default='', prompt='enter the path', help=f'path to store private key')
-@click.option('--path_to_the_file', default='', prompt='enter the path', help=f'path to file')
-@click.option('--path_to_the_signature', default='', prompt='enter the path', help=f'signature path')
-def sign_file(path_to_private_key, path_to_the_file, path_to_the_signature):
-    _path_to_private = sanitaize_path(path_to_private_key, PRIVATE_KEY)
-    _path_to_the_file = sanitaize_path(path_to_the_file, FILE)
-    _path_to_the_signature = sanitaize_path(path_to_the_signature, ELECTRONIC_SIGNATURE)
-
-    h = SHA256.new()
-    with open(_path_to_the_file, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            h.update(chunk)
-
-    try:
-        f = open(_path_to_private, 'r')
-        key = RSA.import_key(f.read())
-    except FileNotFoundError:
-        raise FileNotFoundError('Private key not found')
-
-    signature = pkcs1_15.new(key).sign(h)
-    f = open(_path_to_the_signature, 'wb')
-    f.write(signature)
-    f.close()
-
-    click.echo(f'Signature saved - {_path_to_the_signature}')
-
-
-@click.command()
-@click.option('--path_to_private_key', default='', prompt='enter the path', help=f'path to store private key')
-@click.option('--path_to_public_key', default='', prompt='enter the path', help=f'path to store public key')
+@click.option('-p_private', '--path_to_private_key', default=os.path.join('./', PRIVATE_KEY),
+              prompt='Enter the path to private key', type=click.Path(exists=True, dir_okay=False, readable=True),
+              help=f'Path to store private key')
+@click.option('-p_public', '--path_to_public_key', default=os.path.join('./', PUBLIC_KEY),
+              prompt='Enter the path to public key', type=click.Path(exists=False, dir_okay=False, readable=True),
+              help=f'Path to store public key')
 def get_public_key(path_to_private_key, path_to_public_key):
-    _path_to_private = sanitaize_path(path_to_private_key, PRIVATE_KEY)
-    _path_to_public = sanitaize_path(path_to_public_key, PUBLIC_KEY)
-
     try:
-        f = open(_path_to_private, 'r')
+        f = open(path_to_private_key, 'r')
         key = RSA.import_key(f.read())
     except FileNotFoundError:
         raise FileNotFoundError('Private key not found')
 
     pubkey = key.publickey()
-    f = open(_path_to_public, 'wb')
+    f = open(path_to_public_key, 'wb')
     f.write(pubkey.export_key('PEM'))
     f.close()
 
-    click.echo(f'Public key saved - {_path_to_public}')
+    click.echo(f'Public key saved - {path_to_public_key}')
 
 
 @click.command()
-@click.option('--path', default='', prompt='enter the path', help=f'path to store private key')
+@click.option('-p', '--path', default=os.path.join('./', PRIVATE_KEY), prompt='Enter the path to save private key',
+              type=click.Path(exists=False, dir_okay=False, readable=True), help=f'Path to store private key')
 def get_private_key(path):
-    _path = path if path != '' else PRIVATE_KEY
+    """Create private key"""
     key = RSA.generate(1024, os.urandom)
-    f = open(_path, 'wb')
+    f = open(path, 'wb')
     f.write(key.export_key('PEM'))
     f.close()
 
-    click.echo(f'Private key saved - {_path}')
+    click.echo(f'Private key saved - {path}')
 
 
 @click.group()
+# @click.option('--m', default='', prompt='enter the path', help=f'path to store private key')
 def main():
     pass
 
