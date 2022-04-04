db31f3baf5847457f47f6706fa2015d15a29d0b7
Ярослав Сухоруков <y.sukhorukov@iva-tech.ru>
2022-04-04 23:13:45 +0300

    a1

--- signature/setup.py
+++ signature/setup.py
@@ -1,10 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='signature',
     version='0.1.0',
     py_modules=['signature'],
-    packages=find_packages(),
     install_requires=[
         'Click',
         'pycryptodome'
